da35ef906e2fca7410a1dfd0e2a9acd74bd0fcbc
isaac meira <fspp@outlook.com>
2019-03-06 19:21:21 -0300

    added some text effects

--- js/script.js
+++ js/script.js
@@ -20,6 +20,9 @@ $(document).ready(function () { // 6,32 5,38 2,34
     foregroundBorderWidth: 5,
     backgroundBorderWidth: 1,
     percent: 70,
+    progressColor: 'rgb(94,94,94)',
+    
+    backgroundColor : 'rgb(94,94,94)',
     iconColor: 'coral',
     icon: 'f13b',
     iconSize: '40',
@@ -31,6 +34,7 @@ $(document).ready(function () { // 6,32 5,38 2,34
     animationStep: 1,
     foregroundBorderWidth: 5,
     backgroundBorderWidth: 1,
+    backgroundColor : 'rgb(94,94,94)',
     percent: 65,
     iconColor: 'coral',
     icon: 'f13c',
@@ -42,6 +46,7 @@ $(document).ready(function () { // 6,32 5,38 2,34
     animation: 1,
     animationStep: 1,
     foregroundBorderWidth: 5,
+    backgroundColor : 'rgb(94,94,94)',
     backgroundBorderWidth: 1,
     percent: 75,
     iconColor: 'coral',
@@ -53,3 +58,36 @@ $(document).ready(function () { // 6,32 5,38 2,34
 
 
 });
+
+// Wrap every letter in a span
+$('.ml3').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    
+    delay: function(el, i) {
+      return 150 * (i+1)
+    }
+  });
+
+  $('.ml4').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: false})
+    .add({
+      targets: '.ml4 .letter',
+      opacity: [0,1],
+      easing: "easeInOutQuad",
+      
+      delay: function(el, i) {
+        return 75 * (i+1)
+      }
+    });
+
+
