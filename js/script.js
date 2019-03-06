255877551bda88427806aee69a0636e49fd22444
isaac meira <fspp@outlook.com>
2019-03-05 23:24:57 -0300

    Added effects, change the layout

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// nice scroll
+
+$("body").niceScroll({
+  cursorcolor: "coral",
+  cursorwidth: "10px",
+  cursorborder: "none",
+  horizrailenabled: false
+
+});
+
+
+// CIRCLE
+
+
+$(document).ready(function () { // 6,32 5,38 2,34
+
+  $("#htmlCircle").circliful({
+    animation: 1,
+    animationStep: 6,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 1,
+    percent: 70,
+    iconColor: 'coral',
+    icon: 'f13b',
+    iconSize: '40',
+    iconPosition: 'middle',
+    foregroundColor: 'coral'
+  });
+  $("#cssCircle").circliful({
+    animation: 1,
+    animationStep: 6,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 1,
+    percent: 65,
+    iconColor: 'coral',
+    icon: 'f13c',
+    iconSize: '40',
+    iconPosition: 'middle',
+    foregroundColor: 'coral'
+  });
+  $("#jsCircle").circliful({
+    animation: 1,
+    animationStep: 6,
+    foregroundBorderWidth: 5,
+    backgroundBorderWidth: 1,
+    percent: 75,
+    iconColor: 'coral',
+    icon: 'f12e',
+    iconSize: '40',
+    iconPosition: 'middle',
+    foregroundColor: 'coral'
+  });
+
+
+});
